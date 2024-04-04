900cccf34650bcaa23f64d74728789241166b439
dheeraj404 <130442066+dheeraj404@users.noreply.github.com>
2024-04-04 17:34:16 +0530

    first commit

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def classify_banana():
     return jsonify({'prediction': prediction[0]})
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
